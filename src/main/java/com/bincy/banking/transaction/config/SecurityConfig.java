a165b635a035be843e794215f0f957a7ce752c9d
BincyNizam-lab <144982528+BincyNizam-lab@users.noreply.github.com>
2026-05-21 17:07:16 +0300

    Implemented JWT authentication filter and protected APIs

--- src/main/java/com/bincy/banking/transaction/config/SecurityConfig.java
+++ src/main/java/com/bincy/banking/transaction/config/SecurityConfig.java
@@ -1,29 +1,57 @@
 package com.bincy.banking.transaction.config;
 
 
+import com.bincy.banking.transaction.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.
+        SessionCreationPolicy;
 
+import org.springframework.security.web.
+        authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(
+            JwtAuthenticationFilter jwtFilter) {
+
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) {
 
         http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        ))
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**",
+                        .requestMatchers(
+                                "/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**")
+                                "/v3/api-docs/**"
+                        )
                         .permitAll()
+
                         .anyRequest()
                         .authenticated()
                 )
+
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
